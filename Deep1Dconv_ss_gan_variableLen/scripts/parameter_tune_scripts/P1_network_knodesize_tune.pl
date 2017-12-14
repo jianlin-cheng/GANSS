3f2f97464242ebb2fc43ca19d952a406a267192d
huge200890 <https://huge200890@github.com>
2017-12-14 16:34:08 -0600

    Add the parameter tunning scripts for variable-lenght GAN

---
+++ Deep1Dconv_ss_gan_variableLen/scripts/parameter_tune_scripts/P1_network_knodesize_tune.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+use POSIX;
+
+if (@ARGV != 2 ) {
+  print "Usage: <input> <output>\n";
+  exit;
+}
+
+
+$outputdir = $ARGV[0];
+$sbatch_folder = $ARGV[1];
+
+$c=0;
+for($i=30;$i<=50;$i+=2)
+{
+  
+  $c++;
+  print "\n\n###########  processing kmax size $i\n";
+
+  $runfile="$sbatch_folder/P1_run_sbatch_$c.sh";
+  print "Generating $runfile\n";
+  open(SH,">$runfile") || die "Failed to write $runfile\n";
+  
+
+  print SH "#!/bin/bash -l\n";
+  print SH "#SBATCH -J  ST1_km-g-$c\n";
+  print SH "#SBATCH -o ST1_kmg-g-$c-%j.out\n";
+  print SH "#SBATCH -p Lewis,gpu3\n";
+  print SH "#SBATCH -N 1\n";
+  print SH "#SBATCH -n 1\n";
+  print SH "#SBATCH --mem 30G\n";
+  #print SH "#SBATCH --gres gpu:1\n";
+  print SH "#SBATCH -t 2-00:00:00\n";
+  
+  
+  #print SH "## Load Needed Modules\n";
+  #print SH "module load cuda/cuda-8.0\n";
+  
+  #print SH "## Force Load the Blacklisted Driver\n";
+  #print SH "export GPUARRAY_FORCE_CUDA_DRIVER_LOAD=\"\"\n";
+  
+  print SH "## Activate python virtual environment\n";
+  print SH "source /storage/htc/bdm/tools/python_virtualenv/bin/activate\n";
+
+  print SH "datadir=/storage/htc/bdm/jh7x3/DeepCov_QA_revision/Training_packages_complex_rosetta/SingleDomain_proteins_training/Train_data_20171020\n";
+  print SH "outputdir=$outputdir\n";
+
+  print SH "echo \"#################  Training on inter 15\"\n";
+  print SH "THEANO_FLAGS=floatX=float32,device=cpu python /storage/htc/bdm/jh7x3/DeepCov_QA_revision/Training_packages_complex_rosetta/Full_proteins_training/training_main_iterative_strategy1_20171019_auto_globalonly_withoutRosetta.py 15 5 5   nadam '6' $i 50 50 3  \$datadir \$outputdir\n";
+  close SH;
+
+
+  close SH;
+
+
+}
